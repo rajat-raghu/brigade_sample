3459815b05a497046b111b4b3f5032b91e302302
root <rjraghu805@gmail.com>
2020-08-05 19:08:48 +0530

    brigade

--- brigade.js
+++ brigade.js
@@ -5,3 +5,25 @@ events.on("push", function(e, project) {
 })
 
 
+
+  // Create a new job
+  var testRunner = new Job("test-runner")
+
+  // We want our job to run the stock Docker Python 3 image
+  testRunner.image = "python:3"
+
+  // Now we want it to run these commands in order:
+  testRunner.tasks = [
+    "cd /src/app",
+    "pip install -r flask",
+    "cd /src/",
+    "python setup.py test"
+  ]
+
+  // Display logs from the job Pod
+  testRunner.streamLogs = true;
+
+  // We're done configuring, so we run the job
+  testRunner.run()
+})
+
