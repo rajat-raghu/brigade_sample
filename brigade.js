977f2893c6673164afe88cb4714297f05b85d7fa
root <rjraghu805@gmail.com>
2020-08-06 17:07:00 +0530

    final

--- brigade.js
+++ brigade.js
@@ -1,32 +1,3 @@
-const { events } = require("brigadier");
-
-events.on("push", function(e, project) {
-  console.log("received push for commit " + e.revision.commit)
-})
-
-
-
-  // Create a new job
-  var testRunner = new Job("test-runner")
-
-  // We want our job to run the stock Docker Python 3 image
-  testRunner.image = "python:3"
-
-  // Now we want it to run these commands in order:
-  testRunner.tasks = [
-    "cd /src/app",
-    "pip install -r flask",
-    "cd /src/",
-    "python setup.py test"
-  ]
-
-  // Display logs from the job Pod
-  testRunner.streamLogs = true;
-
-  // We're done configuring, so we run the job
-  testRunner.run()
-})
-
 const { events, Job } = require("brigadier");
 
 // GitHub Check events to watch for
@@ -104,3 +75,4 @@ function checkRequested(e, p) {
     return end.run()
   });
 }
+
